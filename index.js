e63f2a18d2d7680d11085fd9c7102f58d7edc103
Kevin <kmontealegremelo@gmail.com>
2020-09-07 19:32:39 -0500

    npm init

--- index.js
+++ index.js
@@ -11,3 +11,23 @@ appServer.listen (3000, ()=>{
     console.log('SERVER IS RUNNING ON PORT 3000');
 });
 
+
+// 3. Mostrar todos los videojuegos
+
+appServer.get ('/listaVideojuegos',
+    (req, res) => {
+        res.json(listGames)
+    }
+);
+
+// 4. Traer un usuario por id enviado como parámetro
+
+appServer.get ('/getUser/:idGame' , (req, res)=>{
+    const gameId = req.params.idGame
+    listUsers.forEach(function (element, index)  {
+        if(element.id == gameId) {
+            console.log("Encontro el videojuego por id")
+            res.json(element)
+        }
+    });
+});
