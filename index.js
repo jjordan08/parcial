6e06d8269ee5068c2984cedd2b9772a07b35a327
jjordan08 <jjordan_08@hotmail.com>
2020-09-07 19:33:38 -0500

    punto 1 y 2

--- index.js
+++ index.js
@@ -5,12 +5,35 @@ const myUser = require ('./models/game');
 
 appServer.use(express.json());
 
-var listUsers = []
+var listGames = []
 
 appServer.listen (3000, ()=>{
     console.log('SERVER IS RUNNING ON PORT 3000');
 });
 
+// 1. Crear un nuevo juego
+
+appServer.post ('/newgame' , (req, res)=>{
+
+    // Agregamos el juego al arreglo
+
+    listGames.push(req.body)
+
+    res.send ('Game Added');
+});
+
+// 2. Eliminar un juego por id enviado como parámetro
+
+appServer.post ('/deleteGame/:idGame' , (req, res)=>{
+    const id = req.params.id
+    listGames.forEach(function (element, index)  {
+        if(element.id == id) {
+            console.log("Se elimino el juego por ID")
+            listUsers.splice(index, 1)
+        }
+    });
+    res.send ('List Updated');
+});
 
 // 3. Mostrar todos los videojuegos
 
